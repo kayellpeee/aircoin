43b25e6008a1707117fe043e2b82a04f725ad322
Keenan Lidral-Porter <klp2012@gmail.com>
2014-08-24 16:35:22 -0700

    creates file for common utility functions

---
+++ utilities.js
@@ -0,0 +1,25 @@
+var db = require('./app/config');
+var User = require('./app/models/user');
+var Wallet = require('./app/models/wallet');
+var Users = require('./app/collections/users');
+var Wallets = require('./app/collections/wallets');
+
+exports.findUser = function(phone, password){
+
+}
+
+exports.findWallet = function(user_id){
+
+}
+
+exports.createUser = function(phone, password){
+
+}
+
+exports.createWallet = function(user_id){
+
+}
+
+exports.deleteWallet = function(user_id){
+
+}
